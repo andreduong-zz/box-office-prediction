895d0c63b59e511688f1e64ddbd7a52491c13ee0
Andre Duong <andreduong.07@gmail.com>
2020-01-09 17:51:12 -0600

    Updates

---
+++ precache-manifest.586c23c65e68ef51c5a0c1d9a0fe9943.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f975949250ebe0d9b7c1",
+    "url": "/react-stocks/static/css/main.d43fd375.chunk.css"
+  },
+  {
+    "revision": "f975949250ebe0d9b7c1",
+    "url": "/react-stocks/static/js/main.ddec1634.chunk.js"
+  },
+  {
+    "revision": "5b43932ebd4906977ac8",
+    "url": "/react-stocks/static/js/runtime~main.d4cf2f73.js"
+  },
+  {
+    "revision": "28c16e147cbaa3c7da78",
+    "url": "/react-stocks/static/css/2.c1095a52.chunk.css"
+  },
+  {
+    "revision": "28c16e147cbaa3c7da78",
+    "url": "/react-stocks/static/js/2.c7642b0e.chunk.js"
+  },
+  {
+    "revision": "beab86f2597b04d6e04f671a7a84fa2c",
+    "url": "/react-stocks/static/media/react-stocks.beab86f2.svg"
+  },
+  {
+    "revision": "e539d5e718b6f262bfa1122f7a4b12df",
+    "url": "/react-stocks/static/media/react-stocks-land.e539d5e7.svg"
+  },
+  {
+    "revision": "823b6bf8d82337ebd6055362aa7d3ac3",
+    "url": "/react-stocks/index.html"
+  }
+];
